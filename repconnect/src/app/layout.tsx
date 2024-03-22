4ef1310b9eab653ec7542d47cfce05cfa7291b25
Mamad Jadali <jadalimamad@gmail.com>
2024-03-23 03:14:00 +0330

    Header,Footer

--- repconnect/src/app/layout.tsx
+++ repconnect/src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { DM_Sans} from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 
 const dmSans = DM_Sans({
@@ -11,13 +13,17 @@ const dmSans = DM_Sans({
 });
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={dmSans.variable}>
-      <body className="">{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+        <body className="bg-[#070815] text-white">
+            <Header/>
+            <main>{children}</main>
+            <Footer/>
+          <PrismicPreview repositoryName={repositoryName} />
+        </body>
     </html>
   );
 }
---
+++ repconnect/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import WordMark from "@/components/WordMark";
+
+export default function Header() {
+    return (
+        <div><WordMark/>
+            <h3 className="font-bold underline">header</h3>
+        </div>
+    );
+}
+
