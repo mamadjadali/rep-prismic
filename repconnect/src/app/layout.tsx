cec4c6b574b5cece97558d39efc94a176155f607
Mamad Jadali <jadalimamad@gmail.com>
2024-03-23 02:13:36 +0330

    tailwind, prettier

--- repconnect/src/app/layout.tsx
+++ repconnect/src/app/layout.tsx
@@ -1,14 +1,22 @@
+import "./globals.css";
+import { DM_Sans} from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+
+const dmSans = DM_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-dm-sans',
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={dmSans.variable}>
+      <body className="">{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
