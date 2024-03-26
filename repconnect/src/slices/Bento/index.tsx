a41f6040468da85fbf994efeace30cf5b125d3b4
Mamad Jadali <jadalimamad@gmail.com>
2024-03-26 08:16:06 +0330

    bento media

--- repconnect/src/slices/Bento/index.tsx
+++ repconnect/src/slices/Bento/index.tsx
@@ -52,7 +52,7 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
               <div className="max-w-md text-balance text-slate-300">
                 <PrismicRichText field={item.body} />
               </div>
-              <PrismicNextImage field={item.image} className="max-h-36 w-auto" />
+              <PrismicNextImage field={item.image} className="h-auto" />
             </div>
         ))}
       </div>
