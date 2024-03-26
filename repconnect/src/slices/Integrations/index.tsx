1194fd76c62c7f0f63b71786241aacb700e8ea36
Mamad Jadali <jadalimamad@gmail.com>
2024-03-27 02:17:53 +0330

    integrations gsap

--- repconnect/src/slices/Integrations/index.tsx
+++ repconnect/src/slices/Integrations/index.tsx
@@ -9,6 +9,7 @@ import { SiWireguard, SiFortinet, SiMikrotik, SiDell, SiCisco, SiVmware } from "
 
 import StylizedLogoMark from "@/slices/Integrations/StylizedLogoMark";
 import clsx from "clsx";
+import AnimatedContent from "@/slices/Integrations/AnimatedContent";
 
 /**
  * Props for `Integrations`.
@@ -19,15 +20,6 @@ export type IntegrationsProps = SliceComponentProps<Content.IntegrationsSlice>;
  * Component for "Integrations" Slices.
  */
 const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
-  const icons = {
-    cisco: <SiCisco/>,
-    mikrotik: <SiMikrotik/>,
-    wireguard: <SiWireguard/>,
-    dell: <SiDell/>,
-    Fortinet: <SiFortinet/>,
-    vm: <SiVmware/>,
-  };
-
   return (
     <Bounded
         data-slice-type={slice.slice_type}
@@ -50,33 +42,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
         <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300">
           <PrismicRichText field={slice.primary.body} />
         </div>
-
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.items.map((item, index) => (
-              <React.Fragment key={index}>
-                {index === Math.floor(slice.items.length / 2) && (
-                    <>
-                      <StylizedLogoMark />
-                      <div className="signal-line rotate-180 bg-gradient-to-t" />
-                    </>
-                )}
-                <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-                  {item.icon && icons[item.icon]}
-                </div>
-                {index !== slice.items.length - 1 && (
-                    <div
-                        className={clsx(
-                            "signal-line",
-                            index >= Math.floor(slice.items.length / 2)
-                                ? "rotate-180"
-                                : "rotate-0",
-                        )}
-                    />
-                )}
-              </React.Fragment>
-          ))}
-        </div>
-
+        <AnimatedContent slice={slice}/>
       </div>
     </Bounded>
   );
